e12ab75045bb428a982ebecd38703742032b706f
Ahmed Raza <raza2393@gmail.com>
2020-04-25 19:19:38 +0530

    Changing x cordinates on using left and right arrow keys

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+           
+            pos.x -= speed * Time.deltaTime;
+        }
+        else  if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+            
+        }
+        this.transform.position = pos;
+    }
+}
